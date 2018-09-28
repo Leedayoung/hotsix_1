88d6c8801272c54b4d31fbbc475a81870089ec3b
PulseKim <30708572+PulseKim@users.noreply.github.com>
2018-09-28 16:44:55 +0900

    temp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,50 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
+#include <vector>
 using namespace std;
-void renderScene(void) {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glFlush();
-}
+
+class Map{
+private:
+	enum map_info{wall, empty, item};
+	int map_size;
+	int** map_arr;
+	vector<Enemy> enem_vec;
+
+public:
+	Map(){
+		init_window();
+		map_init();
+		object_init();
+	}
+	void init_window() {
+
+	}
+	void map_init() {
+		
+	}
+	void object_init() {
+		int tempx, tempy;
+		for (int i = 0; i < enem_vec.size(); ++i) {
+			while (true) {
+				tempx = rand() % map_size;
+				tempy = rand() % map_size;
+				bool flag = true;
+				if (map_arr[tempx][tempy] != wall) {
+					for (int j = 0; j < i; ++j) {
+						if (enem_vec[j].get_x == tempx && enem_vec[j].get_y == tempy)
+							flag = false;
+					}
+					if (flag) {
+						enem_vec.push_back(i,tempx,tempy);
+					}
+				}
+			}
+		}
+		Player(posx, posy);
+	}
+};
+
 void main(int argc, char **argv) {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(320, 320);
-	glutCreateWindow("Hello OpenGL");
-	glutDisplayFunc(renderScene);
-	glClearColor(1, 1, 1, 1);
-	glewInit();
-	glutMainLoop();
+
 }
